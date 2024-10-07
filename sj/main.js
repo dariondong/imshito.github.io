d7e9a39e0615a6433919f85a56aa1b5526164f95
Darion <mc081208@163.com>
2024-10-07 00:27:40 +0000

    4.0.4

--- sj/main.js
+++ sj/main.js
@@ -58,7 +58,7 @@ function ptc(){
         //清除定时器
         clearInterval(times);
         // 停止以后，也要修改span的状态和文字
-        document.getElementById('btngroup').innerHTML="<button class=\"layui-btn\" id=\"btnpt\" onclick=\"ptc()\">普通摇</button> <button class="layui-btn" id="btnpt"><a href="/sj/index.html">刷新</a></button>"
+        document.getElementById('btngroup').innerHTML="<button class=\"layui-btn\" id=\"btnpt\" onclick=\"ptc()\">普通摇</button> <button class=\"layui-btn\" id=\"btnpt\"><a href=\"/sj/index.html\">刷新</a></button>"
        
         status1 = true;
     }
